32fd8b04b31cb92a7d9ee2e5e61d757a85025659
s4026730 <s4026730@rmit.edu.vn>
2026-04-23 22:26:52 +0700

    Minor updates on the contents of main and home files.

--- bidblitz/src/main/java/com/example/bidblitz/Main.java
+++ bidblitz/src/main/java/com/example/bidblitz/Main.java
@@ -30,29 +30,8 @@ public class Main extends Application {
         else{
             System.out.println("Error: Incorrect Image Path.");
         }
-        stage.setTitle("BitBlitz");
+        stage.setTitle("BidBlitz");
         stage.setScene(mainScene);
         stage.show();
     }
-
-    /* public class ScreenController {
-        private HashMap<String, Pane> screenMap = new HashMap<>();
-        private Scene main;
-
-        public ScreenController(Scene main) {
-            this.main = main;
-        }
-
-        protected void addScreen(String name, Pane pane){
-            screenMap.put(name, pane);
-        }
-
-        protected void removeScreen(String name){
-            screenMap.remove(name);
-        }
-
-        protected void activate(String name){
-            main.setRoot(screenMap.get(name) );
-        }
-    } */
 }
