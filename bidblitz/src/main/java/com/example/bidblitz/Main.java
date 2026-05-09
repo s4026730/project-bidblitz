c8ba08ad6971414fd62edce42e1248fe1f18a168
s4026730 <s4026730@rmit.edu.vn>
2026-05-09 18:06:36 +0700

    Finish the Sign Up function layout for Sign Up page and replace the directory name of applicationImage to image.

--- bidblitz/src/main/java/com/example/bidblitz/Main.java
+++ bidblitz/src/main/java/com/example/bidblitz/Main.java
@@ -4,11 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,8 +14,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("guest-main-view.fxml"));
         Scene mainScene = new Scene(fxmlLoader.load());
-        mainScene.getStylesheets().add(getClass().getResource("/css/General.css").toExternalForm());
-        InputStream iconStream = getClass().getResourceAsStream("/applicationImages/bidblitz-icon.png");
+        InputStream iconStream = getClass().getResourceAsStream("/image/bidblitz-icon.png");
 
         if (iconStream != null){
             Image icon = new Image(iconStream);
