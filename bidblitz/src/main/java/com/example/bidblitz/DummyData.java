8097ae2994a801ee526cf636f2c52dfa234696a0
buiquochuy113vt@gmail.com <buiquochuy113vt@gmail.com>
2026-05-20 02:43:23 +0700

    Implement auction details section in user auction view and Add dummy auction data for testing.

---
+++ bidblitz/src/main/java/com/example/bidblitz/DummyData.java
@@ -0,0 +1,9 @@
+package com.example.bidblitz;
+
+import com.example.bidblitz.auction.Auction;
+
+public class DummyData {
+    public static Auction createTestAuction(){
+        return null;
+    }
+}
